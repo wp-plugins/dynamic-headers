cbe4ebf5a7f6d2701510435cf63a42f06e12a4ef
k0pper <k0pper@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2009-04-28 20:50:03 +0000

    tagging version 2.3 bug fixes
    git-svn-id: https://plugins.svn.wordpress.org/dynamic-headers/tags/2.3@114147 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- admin/header.php
+++ admin/header.php
@@ -1,9 +1,9 @@
 <?php
-	$path = ABSPATH."wp-content/plugins/custom-header/header-images/";
+	$path = ABSPATH."wp-content/plugins/dynamic-headers/header-images/";
 	
 	if(is__writable($path) != true){
 		echo '<div class="error"><p>';
-			echo 'It looks like <strong>wp-content/plugins/custom-header/header-images/</strong> is not writable.<br /><br /> You will need to make this directory writable in order for the plugin to work.';
+			echo 'It looks like <strong>wp-content/plugins/dynamic-headers/header-images/</strong> is not writable.<br /><br /> You will need to make this directory writable in order for the plugin to work.';
 		echo '</p></div><br />';
 	}
 ?>
